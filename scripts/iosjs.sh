f087925a9e23593dd2a91d5024bd23458f71944b
COREY SHAW <Corey.Shaw@libertymutual.com>
2017-12-19 10:17:15 -0500

    Adding script for build

---
+++ scripts/iosjs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Build source location: $BUILD_SOURCESDIRECTORY"
+cd $BUILD_SOURCESDIRECTORY/templates/HelloWorld
+
+mkdir -p build
+export DEV_MODE=1
+# The Template (HelloWorld)
+
+npm install -g react-native-cli
+
+yarn install
+npm run test
+
+#Installing dependencies
+sudo gem install fastlane -NV
+
+#Install python
+brew install python
+curl -O http://python-distribute.org/distribute_setup.py
+python distribute_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+pip install cobertura-clover-transform
+
+cd ios
+bundle install
+bundle exec fastlane qa_app
